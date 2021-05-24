76f8dc755cf7bfa78a0166872159a950fff18b81
JosuaKugler <josua.kugler@gmail.com>
2021-05-24 14:21:39 +0200

    read watchparty locations from json and add .idea folder to .gitignore

--- docs/map/locations.js
+++ docs/map/locations.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView({lat: 49.39505, lon: 8.6759873}, 13);
+var map = L.map('map').setView({lat: 49.43, lon: 8.6759873}, 12);
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -8,8 +8,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // show the scale bar on the lower left corner
 L.control.scale().addTo(map);
 
+$.getJSON("data.json", data => {
+    for (key in data) {
+        value = data[key];
+        console.log(value);
+        L.marker({lat: value.lat, lon: value.lon}).bindPopup(value.name).addTo(map);
+    }
+})
+
 // show a marker on the map
-L.marker({lat: 49.40824, lon: 8.6714711}).bindPopup('Calvary Chapel').addTo(map);
-L.marker({lat: 49.4085374, lon: 8.6808019}).bindPopup('Hosanna Gemeinde').addTo(map);
-L.marker({lat: 49.3648173, lon: 8.678083}).bindPopup('Freie Evangelische Gemeinde').addTo(map);
+//L.marker({lat: 49.40824, lon: 8.6714711}).bindPopup('Calvary Chapel').addTo(map);
 
