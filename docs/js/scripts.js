25c2d733f61dd48f00427d9dca5db4e82d8a34c5
JoniDepp47 <jonathan47.schaefer@gmail.com>
2021-05-27 12:54:11 +0200

    playing with picture of Mihamm

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -61,3 +61,20 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// fade in/out picture of Mihamm from https://stackoverflow.com/questions/48202389/fade-image-on-scrolling
+/* <script type="text/javascript">
+  $(function(){
+  var element = $('.top');
+  $(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+  element.fadeIn();
+}
+  else {
+  element.fadeOut();
+}
+});
+});
+</script>
+<img th:src="@{/img/bc.jpg}" class="top"/> */
